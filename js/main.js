d591058a2f9c1ea21be88cda9db8195ace6fb6f6
Dancan Chibole <dancankingstar@gmail.com>
2020-12-01 12:12:54 -0800

    Third commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// let navBar = $(".navbar");
+// $(window).scroll(function () {
+//   let oTop = $("#second").offset().top - window.innerHeight;
+//   if ($(window).scrollTop() > oTop) {
+//     navBar.addClass("sticky");
+//   } else {
+//     navBar.removeClass("sticky");
+//   }
+// });
